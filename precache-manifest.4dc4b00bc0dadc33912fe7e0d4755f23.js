d18c208299b7d582f4bdb400817090355e7d8ed6
Chan Wan <epp_tech_chanwan@CHANWAN.local>
2019-11-10 17:12:41 +0800

    Updates

---
+++ precache-manifest.4dc4b00bc0dadc33912fe7e0d4755f23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ba902872c7ab0ea2653fd1027738bb",
+    "url": "/campaign-lister/index.html"
+  },
+  {
+    "revision": "83eebcf83295c28e15cf",
+    "url": "/campaign-lister/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "471e6f90051d94736a6c",
+    "url": "/campaign-lister/static/css/main.3fab52f0.chunk.css"
+  },
+  {
+    "revision": "83eebcf83295c28e15cf",
+    "url": "/campaign-lister/static/js/2.23b63912.chunk.js"
+  },
+  {
+    "revision": "471e6f90051d94736a6c",
+    "url": "/campaign-lister/static/js/main.f36bfb9f.chunk.js"
+  },
+  {
+    "revision": "9024d632e2942ddabc7f",
+    "url": "/campaign-lister/static/js/runtime-main.d944d05b.js"
+  },
+  {
+    "revision": "d40462ed73ad0ca09608933167c192e2",
+    "url": "/campaign-lister/static/media/logo.d40462ed.png"
+  }
+]);
